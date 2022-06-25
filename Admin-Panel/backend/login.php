6555f2b3eb39762cadfb148b7c0b1dc0bd0b1873
Emmanuel <emmanuel.nocum@cvsu.edu.ph>
2022-06-25 21:43:20 +0800

    dashboard-with-logout

--- Admin-Panel/backend/login.php
+++ Admin-Panel/backend/login.php
@@ -7,23 +7,27 @@ include_once("../connections/connection.php");
   
 $con = connection();
 
-
-  if(isset($_POST['username'])){
-  $username =  mysqli_real_escape_string($con, $_POST['username']);
-  $password =   mysqli_real_escape_string($con,$_POST['password']);
+try{
+  if(isset($_POST['Email'])){
+    $email =  mysqli_real_escape_string($con, $_POST['Email']);
+    $password =   mysqli_real_escape_string($con,$_POST['Password']);
+    
+    $sql = "SELECT * FROM tbl_admin WHERE email = '$email' AND password ='$password' AND status = 'active'";
+    
+    $user = $con ->query($sql) or die ($con->error);
+    $row = $user->fetch_assoc();
+    $total = $user->num_rows;
   
-  $sql = "SELECT * FROM tbl_admin WHERE email = '$username' AND password ='$password' AND status = 'active'";
-  
-  $user = $con ->query($sql) or die ($con->error);
-  $row = $user->fetch_assoc();
-  $total = $user->num_rows;
-
-  if($total > 0){
-  $_SESSION['ID'] = $row['id'];
-    $_SESSION['Position'] = $row['position'];
-    echo json_encode(array("statusCode"=>200));
-  }else{
-    echo json_encode(array("statusCode"=>201));
-   }
-  }
+    if($total > 0){
+    $_SESSION['Email'] = $row['email'];
+      $_SESSION['Position'] = $row['position'];
+      echo json_encode(array("statusCode"=>200));
+    }else{
+      echo json_encode(array("statusCode"=>201));
+     }
+    }
+}catch(Exception $e){
+  echo json_encode(array("statusCode"=>$e->getMessage()));
+}
+ 
 ?>
